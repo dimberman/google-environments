22dcf56336446a208f5b9141372ce04641b7819a
Drone CI <steven@astronomer.io>
2019-10-22 12:47:22 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.540: Update main.tf (#50)

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.535"
+  version = "0.1.540"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
