48f72c719f77cf90736c52a14a90024e69b64f1f
Drone CI <steven@astronomer.io>
2019-10-24 14:52:27 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.561: Fix probe settings (#54)
    * Fix probe settings

--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.555"
+  version = "0.1.561"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
