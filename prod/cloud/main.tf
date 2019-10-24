81eb801fb173f9c76034df78ba409a868fa6cbfa
Drone CI <steven@astronomer.io>
2019-10-24 15:27:13 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.566: Rollback tiller to 2.14.1 (#55)
    * Rollback tiller to 2.14.1

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.561"
+  version = "0.1.566"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
