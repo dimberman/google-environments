49890f737c3bb5306f7a8d6073278c52ae3ecd9b
Drone CI <steven@astronomer.io>
2019-10-25 21:08:18 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.610: Drone CI: Update helm chart to version v0.10.3-rc.2

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.608"
+  version = "0.1.610"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
