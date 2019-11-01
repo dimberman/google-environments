4a89a3ba69b8e38b5f3f67af67e9dadd852d428e
Steven Miller <sjmiller609@gmail.com>
2019-11-01 18:23:46 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.651: Fix alerts url

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.649"
+  version = "0.1.651"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
