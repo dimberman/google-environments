2f51109abca44f1c6b6331123dd3cd307a6f2b19
Kaxil Naik <kaxilnaik@gmail.com>
2019-11-06 21:31:12 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.675: Update system-components to 0.1.15 (#69)

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.671"
+  version = "0.1.675"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
