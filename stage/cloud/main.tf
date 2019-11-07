c089e8739ba9d79f2df1fa26c2b8a2ca84387566
sjmiller609 <sjmiller609@gmail.com>
2019-11-07 19:38:52 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.681: Fix pagerduty config (#70)

--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.677"
+  version = "0.1.681"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
