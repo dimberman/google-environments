0d26fb78e04ce29af34a50110a544ca2f0adf6bc
sjmiller609 <sjmiller609@gmail.com>
2019-12-04 17:08:53 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.766: Increase Prometheus requests to 32Gi

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.764"
+  version = "0.1.766"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
