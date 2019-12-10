2329093b045af428128c64a835fb89820fe0a20c
Kaxil Naik <kaxilnaik@gmail.com>
2019-12-10 19:50:23 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.790: Drone CI: Update GCP infra module to 1.0.207

--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.788"
+  version = "0.1.790"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
