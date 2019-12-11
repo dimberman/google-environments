821479ef9f92abf757e78577d58e27582dc615d3
sjmiller609 <sjmiller609@gmail.com>
2019-12-11 15:10:17 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.798: Increase elasticsearch data replicas to 8 (#87)

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.794"
+  version = "0.1.798"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
