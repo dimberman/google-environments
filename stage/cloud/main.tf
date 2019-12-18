1e0e1a5b5eb89fe6c0a2ac1f85fdac578c4242cb
Steven Miller <sjmiller609@gmail.com>
2019-12-18 19:54:45 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.846: Fix pod annotations

--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.844"
+  version = "0.1.846"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
