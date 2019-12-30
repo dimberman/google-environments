14563cb623f036d3c1c146ae8291c4babb783835
sjmiller609 <sjmiller609@gmail.com>
2019-12-30 20:16:43 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.866: Prometheus persistence to 500GB and 12 es-data replicas (#97)

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.862"
+  version = "0.1.866"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
