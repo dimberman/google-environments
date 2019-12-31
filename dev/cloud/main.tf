3097ce39a1e60e8d45c8ff84de008e0f4a4dfa04
sjmiller609 <sjmiller609@gmail.com>
2019-12-31 15:22:03 -0500

    Update dev cloud with new changes (#25)

--- dev/cloud/main.tf
+++ dev/cloud/main.tf
@@ -1,23 +1,23 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.649"
+  version = "0.1.868"
 
-  deployment_id          = var.deployment_id
-  email                  = "steven@astronomer.io"
-  zonal_cluster          = true
-  management_api         = "public"
-  enable_gvisor          = false
-  enable_kubecost        = false
-  kubeconfig_path        = var.kubeconfig_path
-  do_not_create_a_record = false
-  lets_encrypt           = true
-  base_domain            = var.base_domain
-  kube_version_gke       = "1.14.7-gke.14"
-  dns_managed_zone       = "steven-zone"
-  enable_velero          = false
-  enable_knative         = true
-  tiller_version         = "2.15.2"
+  deployment_id                = var.deployment_id
+  email                        = "steven@astronomer.io"
+  zonal_cluster                = true
+  management_api               = "public"
+  enable_gvisor                = false
+  enable_kubecost              = false
+  kubeconfig_path              = var.kubeconfig_path
+  do_not_create_a_record       = false
+  lets_encrypt                 = true
+  base_domain                  = var.base_domain
+  dns_managed_zone             = "steven-zone"
+  enable_velero                = false
+  enable_knative               = true
+  enable_gke_metered_billing   = true
+  create_dynamic_pods_nodepool = true
 
   worker_node_size = local.worker_node_size
   db_instance_size = local.db_instance_size
