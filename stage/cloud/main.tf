1d74c1ec53262f203d6996a333e5af27111fc477
Kaxil Naik <kaxilnaik@gmail.com>
2020-01-06 17:25:24 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.891: Add tolerations and affinity on fluentd Daemonset (#74)
    * Add tolerations and affinity on fluentd Daemonset
    
    Based on the change in https://github.com/astronomer/helm.astronomer.io/pull/241
    
    * Update version when available
    
    * Remove redundant nodeAffinity
    
    * Update Astronomer version
    
    * Remove old section from config
    
    * Specify pod annotations on es-client
    
    * Update Prometheus configuration
    
    * Use release branch
    
    Co-authored-by: sjmiller609 <sjmiller609@gmail.com>

--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.878"
+  version = "0.1.891"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
