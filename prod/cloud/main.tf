14b0cbe170b511ab948cbb53879b78b95686d700
Greg Neiheisel <greg@astronomer.io>
2020-01-08 21:54:35 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.956: Bump Airflow Chart to v0.11.0-rc.4 (#105)

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.952"
+  version = "0.1.956"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
