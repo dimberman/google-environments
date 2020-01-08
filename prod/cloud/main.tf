f25e33ac7a2b3e15dc203ee3559cfe0ed11c23f2
sjmiller609 <sjmiller609@gmail.com>
2020-01-08 19:57:18 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.937: Reconfigure ES client (#104)

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.931"
+  version = "0.1.937"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
