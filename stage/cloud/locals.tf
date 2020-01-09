86caa66331d7effa068ec7b1e765599651f96675
sjmiller609 <sjmiller609@gmail.com>
2020-01-09 16:45:33 -0500

    Enable public signups for stage

--- stage/cloud/locals.tf
+++ stage/cloud/locals.tf
@@ -1,6 +1,6 @@
 locals {
   worker_node_size      = "n1-standard-16"
   db_instance_size      = "db-custom-4-15360"
-  public_signups        = false
+  public_signups        = true
   max_worker_node_count = 12
 }
