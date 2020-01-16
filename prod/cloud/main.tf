4adeeae4653ed65d0477fadf56025cbb74369865
Samantha Black (Ingram) <supersamblack@gmail.com>
2020-01-16 19:05:15 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.1007: Add STRIPE_PK to norbit vars (#112)

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.1003"
+  version = "0.1.1007"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
