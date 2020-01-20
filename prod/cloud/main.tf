b38d0c27e76b1ec74a699a08b85356fcbe5ec464
sjmiller609 <sjmiller609@gmail.com>
2020-01-20 01:30:43 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.1033: scale es-data down to 11

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.1031"
+  version = "0.1.1033"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
