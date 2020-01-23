d9a0b771624509478797df33592900010e67ab8e
sjmiller609 <sjmiller609@gmail.com>
2020-01-23 21:39:03 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.1059: Fix fluentd errors

--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.1057"
+  version = "0.1.1059"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
