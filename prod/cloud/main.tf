b8fbff6427589a7c82d4929f08f444ffdab218f5
sjmiller609 <sjmiller609@gmail.com>
2020-01-24 00:01:34 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.1061: turn off canary

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.1059"
+  version = "0.1.1061"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
