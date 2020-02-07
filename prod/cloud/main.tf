7eb29cf40ffb7b8012fdb5744b71e6d21bc0648c
sjmiller609 <sjmiller609@gmail.com>
2020-02-07 01:34:35 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.1097: Fix for canaries (#125)

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.1093"
+  version = "0.1.1097"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
