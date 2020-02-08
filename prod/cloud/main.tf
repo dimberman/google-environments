5399d67af177be81aa185afa2c05626942be6a95
sjmiller609 <sjmiller609@gmail.com>
2020-02-08 19:18:20 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.1111: Double disk size on platform node pool (#127)

--- prod/cloud/main.tf
+++ prod/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.1107"
+  version = "0.1.1111"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
