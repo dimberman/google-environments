bd01ff17cddd4da884c05b81a31cabe3681754fa
Steven Miller <sjmiller609@gmail.com>
2020-02-10 15:43:49 -0500

    Turn off safety

--- bin/terraform_cloud.sh
+++ bin/terraform_cloud.sh
@@ -236,10 +236,6 @@ if [ $TF_AUTO_APPROVE ]; then
     -input=false \
     -out=$PLAN_FILE
 
-  # temporarily, we will disable deploying
-  # so I can avoid messing up staging.
-  exit 0
-
   terraform apply \
     -lock=false \
     -refresh=false \
