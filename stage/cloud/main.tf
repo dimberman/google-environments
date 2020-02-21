1d785942288b2abd8f6cc58e897e0e7b900bbf02
sjmiller609 <sjmiller609@gmail.com>
2020-02-21 17:58:32 +0000

    Drone CI - terraform-google-astronomer-cloud 0.1.1139: Update Astronomer module

--- stage/cloud/main.tf
+++ stage/cloud/main.tf
@@ -1,7 +1,7 @@
 module "astronomer_cloud" {
 
   source  = "astronomer/astronomer-cloud/google"
-  version = "0.1.1137"
+  version = "0.1.1139"
 
   deployment_id          = var.deployment_id
   email                  = "steven@astronomer.io"
